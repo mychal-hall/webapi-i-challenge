01206e7d1533372039808caeb0a03cfc204cb536
Mychal Hall <hallmychal@gmail.com>
2019-05-08 21:06:47 -0500

    Added Read

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express");
 
 const db = require("./data/db.js");
 const server = express();
-const { users } = db;
 
 // MiddleWare
 server.use(express.json());
 
 // Read
 server.get("/api/users", (req, res) => {
-    
-})
+  db
+    .find()
+    .then(allUsers => {
+      res.send(allUsers);
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
+});
 
 // Listening
 server.listen(9000, () => {
