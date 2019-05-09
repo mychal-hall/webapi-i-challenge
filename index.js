a8d28c6222011009e833fcbff691f77681e5db8c
Mychal Hall <hallmychal@gmail.com>
2019-05-08 20:57:01 -0500

    Started the server, listening on 9000

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// implement your API here
+const express = require("express");
+
+const db = require("./data/db.js");
+const server = express();
+const { users } = db;
+
+// MiddleWare
+server.use(express.json());
+
+// Read
+server.get("/api/users", (req, res) => {
+    
+})
+
+// Listening
+server.listen(9000, () => {
+  console.log("Listening on port 9000");
+});
